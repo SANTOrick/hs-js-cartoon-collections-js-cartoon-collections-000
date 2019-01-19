0649cab8581e09c9e28ceb8d46dfc09674442a5c
Riccardo <riccardo_london@live.it>
2019-01-19 12:55:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function dwarfRollCall(dwarves) {
   for (var i = 0; i < dwarves.length; i += 1) {
   let name = dwarves[i];
 line.push((i + 1) + '. ' + name);
+line = line.split('/,').join(' ')
 }
 return line.toString();
 }

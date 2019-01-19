6b41f4b269be5a46ec44be92e661852791272d6d
Riccardo <riccardo_london@live.it>
2019-01-19 12:27:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function dwarfRollCall(dwarves) {
+  for (var i = 0; i < dwarfRollCall.length; i += 1) {
+  let name = dwarfRollCall[i];
+  let line = []
+  line.push(' ' + (i + 1) + '. ' + name);
+}
+return "The line is currently:" + lineOrder;
 }
 
 function summonCaptainPlanet(planeteerCalls){

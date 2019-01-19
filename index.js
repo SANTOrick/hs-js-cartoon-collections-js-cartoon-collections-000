38b9d617597cc67f9df3301426bb4859b92b6ad2
Riccardo <riccardo_london@live.it>
2019-01-19 12:42:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let dwarves =[];
 
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(dwarves=[]) {
     let line = []
   for (var i = 0; i < dwarfRollCall.length; i += 1) {
   let name = dwarfRollCall[i];

dfa4c967bf041db3d5c43ebe9816acef16356330
Riccardo <riccardo_london@live.it>
2019-01-19 12:34:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function dwarfRollCall(dwarves) {
     let line = []
   for (var i = 0; i < dwarfRollCall.length; i += 1) {
   let name = dwarfRollCall[i];
-  let line = []
+  
   line.push(' ' + (i + 1) + '. ' + name);
 }
 return "The line is currently:" + line;
